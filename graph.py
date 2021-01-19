808257db964e4758b9b4f7276514bccf9cf6895d
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-01-19 14:51:03 -0500

    Added test print statement

--- graph.py
+++ graph.py
@@ -5,3 +5,5 @@ def logistic_iter(r, l, d):
     for i in range(d):
         l = logistic_map(r, l)
     return l
+
+print([1-(i/10) for i in range(10)], logistic_iter(3.2, [1-(i/10) for i in range(10)], 20))
