fee98a71f705df9b3416c6f34ea0653003e7b1e3
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2021-01-19 15:12:24 -0500

    Added iteration resolution

--- graph.py
+++ graph.py
@@ -1,6 +1,8 @@
 from PIL import Image
 
-res = 1000
+res = 100
+
+iter = 100
 
 img = Image.new('RGB', (res*3, res))
 
@@ -15,7 +17,7 @@ def logistic_iter(r, l, d):
     return l
 
 for r in [i/res for i in range(res, res*4)]:
-    for i in [j//(1/res) for j in logistic_iter(r, [1-(i/10) for i in range(10)], 20)]:
+    for i in [j//(1/res) for j in logistic_iter(r, [1-(i/iter) for i in range(iter)], 2*iter)]:
         px[r*res-res, res-i-1] = (255,255,255)
 
 img.save('image.png')
